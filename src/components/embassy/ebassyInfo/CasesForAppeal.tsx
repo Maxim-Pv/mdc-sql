6c36b1e7f9de2c0c0c63b5ef24d4b09474abb8db
Maxim <maxim.paw2010@yandex.ru>
2025-10-01 11:52:18 +0300

    main: fix visual bags in embasy page

--- src/components/embassy/ebassyInfo/CasesForAppeal.tsx
+++ src/components/embassy/ebassyInfo/CasesForAppeal.tsx
@@ -1,4 +1,4 @@
-import { useTranslations } from "next-intl";
+import { useTranslations } from 'next-intl';
 
 interface Props {
   title: string;
@@ -13,67 +13,52 @@ type CasesBlock = {
   note?: string;
 };
 
-function CasesCard({ title, items, note, className = "" }: Props) {
+function CasesCard({ title, items, note, className = '' }: Props) {
   const baseStyle = `gap-5 flex flex-col sm:h-[340px] h-auto
 rounded-[25px] shadow-[0px_0px_20px_rgba(0,0,0,0.25)] flex-1 ${className}`;
 
   return (
-    <div className={`${baseStyle} ${"bg-white"}`}>
-      <h4 className="text-[#234BA0] text-[14px] sm:text-[16px] font-semibold px-[20px] sm:px-[30px] pt-[20px]">
+    <div className={`${baseStyle} ${'bg-white'}`}>
+      <h4 className="px-[20px] pt-[20px] text-[14px] font-semibold text-[#234BA0] sm:px-[30px] sm:text-[16px]">
         {title}
       </h4>
-      <div
-        className="bg-[#ED1846] shadow-[0px_0px_20px_rgba(0,0,0,0.25)] rounded-[25px] p-[20px] 
-      sm:max-h-[270px] h-full"
-      >
-        <ul className="list-disc pl-8 text-sm sm:text-base space-y-[6px] sm:space-y-[4px] text-white">
+      <div className="h-full rounded-[25px] bg-[#ED1846] p-[20px] shadow-[0px_0px_20px_rgba(0,0,0,0.25)] sm:max-h-[270px]">
+        <ul className="list-disc space-y-[6px] pl-8 text-sm leading-[18px] text-white sm:space-y-[4px] sm:text-base">
           {items.map((item, idx) => (
             <li key={idx}>{item}</li>
           ))}
         </ul>
-        {note && (
-          <p
-            className={`mt-[20px] font-semibold text-white text-sm sm:text-base`}
-          >
-            {note}
-          </p>
-        )}
+        {note && <p className={`mt-[20px] text-sm font-semibold text-white sm:text-base`}>{note}</p>}
       </div>
     </div>
   );
 }
 
 export default function CasesForAppeal() {
-  const t = useTranslations("embassy.cases");
-  const withAppointment = t.raw("withAppointment") as CasesBlock;
-  const withoutAppointment = t.raw("withoutAppointment") as CasesBlock;
+  const t = useTranslations('embassy.cases');
+  const withAppointment = t.raw('withAppointment') as CasesBlock;
+  const withoutAppointment = t.raw('withoutAppointment') as CasesBlock;
 
   return (
-    <div className="w-full max-w-[1150px] mx-auto">
-      <div className="w-full justify-center flex flex-col items-center gap-[50px] sm:gap-[70px]">
-        <h3 className="text-[20px] sm:text-[30px] font-semibold text-center px-[20px] max-w-[1030px]">
-          {t("title")}
-        </h3>
-        <div className="w-full flex flex-col gap-[20px] text-center justify-center max-w-[1030px] text-sm sm:text-base px-[15px]">
-          <p>{t("desc1")}</p>
-          <p>{t("desc2")}</p>
+    <div className="mx-auto w-full max-w-[1150px]">
+      <div className="flex w-full flex-col items-center justify-center gap-[50px] sm:gap-[70px]">
+        <h3 className="max-w-[1030px] px-[20px] text-center text-[20px] font-semibold sm:text-[30px]">{t('title')}</h3>
+        <div className="flex w-full max-w-[1030px] flex-col justify-center gap-[20px] px-[15px] text-center text-sm sm:text-base">
+          <p>{t('desc1')}</p>
+          <p>{t('desc2')}</p>
         </div>
-        <h3 className="text-[20px] sm:text-[30px] font-semibold text-center px-[20px] max-w-[1030px]">
-          {t("subtitle")}
+        <h3 className="max-w-[1030px] px-[20px] text-center text-[20px] font-semibold sm:text-[30px]">
+          {t('subtitle')}
         </h3>
 
-        <div className="w-full flex flex-col lg:flex-row gap-[30px] px-[20px]">
+        <div className="flex w-full flex-col gap-[30px] px-[20px] lg:flex-row">
           <CasesCard
             title={withAppointment.title}
             items={withAppointment.items}
             note={withAppointment.note}
             className="basis-[55%]"
           />
-          <CasesCard
-            title={withoutAppointment.title}
-            items={withoutAppointment.items}
-            className="basis-[45%]"
-          />
+          <CasesCard title={withoutAppointment.title} items={withoutAppointment.items} className="basis-[45%]" />
         </div>
       </div>
     </div>
--- src/components/embassy/ebassyInfo/ContactInfo.tsx
+++ src/components/embassy/ebassyInfo/ContactInfo.tsx
@@ -1,81 +1,74 @@
-"use client";
+'use client';
 
-import YandexMapDynamic from "@/components/maps/YandexMapDynamic";
-import { ContactContent, CONTACTS } from "@/constant/embassy/contactContent";
-import { IconMail } from "@tabler/icons-react";
-import { useTranslations } from "next-intl";
+import YandexMapDynamic from '@/components/maps/YandexMapDynamic';
+import { ContactContent, CONTACTS } from '@/constant/embassy/contactContent';
+import { IconMail } from '@tabler/icons-react';
+import { useTranslations } from 'next-intl';
 
 const EMBASSY_COORDS: [number, number] = [55.7619244697269, 37.622801894345955];
 
 function OfficeInfo({ data }: { data: ContactContent }) {
-  const t = useTranslations("embassy.contact");
+  const t = useTranslations('embassy.contact');
   const { address, phone, fax, email } = data;
   return (
     <div className="space-y-2 text-xs sm:text-base">
-      <p className="font-medium mb-4 w-[95%] lg:w-full">
-        {" "}
-        {t("addressLabel")} {address}
+      <p className="mb-4 w-[95%] font-medium lg:w-full">
+        {' '}
+        {t('addressLabel')} {address}
       </p>
-      <p className="font-semibold text-base text-[#ED1846]">{phone}</p>
+      <p className="text-base font-semibold text-[#ED1846]">{phone}</p>
       {fax && (
         <p>
-          {t("faxLabel")} {fax}
+          {t('faxLabel')} {fax}
         </p>
       )}
 
       <div
-        className="inline-flex items-center gap-2 px-3 py-2 border rounded-[10px] w-fit"
-        style={{ borderColor: "#234BA0" }}
+        className="inline-flex w-fit items-center gap-2 rounded-[10px] border px-3 py-2"
+        style={{ borderColor: '#234BA0' }}
       >
         <IconMail size={22} strokeWidth={1.5} color="#234BA0" />
-        <span className="font-semibold text-xs sm:text-base text-[#234BA0]">
-          {email}
-        </span>
+        <span className="text-xs font-semibold text-[#234BA0] sm:text-base">{email}</span>
       </div>
     </div>
   );
 }
 
 export default function ContactInfo() {
-  const t = useTranslations("embassy.contact");
+  const t = useTranslations('embassy.contact');
   return (
-    <section className="w-full px-4 py-10 pb-20 bg-[#F3F4F6]">
-      <div className="max-w-[1110px] mx-auto">
-        <h3 className="text-[20px] sm:text-[30px] font-semibold text-center px-[15px] max-w-[1100px] mx-auto">
-          {t("heading")}
+    <section className="w-full px-4 py-10 pb-20">
+      <div className="mx-auto max-w-[1110px]">
+        <h3 className="mx-auto max-w-[1100px] px-[15px] text-center text-[20px] font-semibold sm:text-[30px]">
+          {t('heading')}
         </h3>
 
-        <div className="mt-10 bg-white rounded-[20px] shadow-[0px_0px_20px_rgba(0,0,0,0.25)] flex flex-col-reverse md:flex-row overflow-hidden">
+        <div className="mt-10 flex flex-col-reverse overflow-hidden rounded-[20px] bg-white shadow-[0px_0px_20px_rgba(0,0,0,0.25)] md:flex-row">
           <div className="min-h-[300px] md:w-[50%]">
             <YandexMapDynamic
               coords={EMBASSY_COORDS}
-              name={t("mapName")}
+              name={t('mapName')}
               className="h-[300px] sm:h-[400px] md:h-full"
             />
           </div>
 
-          <div className="p-[15px] md:p-[20px] flex flex-col gap-8 md:w-[50%] text-left">
+          <div className="flex flex-col gap-8 p-[15px] text-left md:w-[50%] md:p-[20px]">
             {CONTACTS.map((c) => (
               <OfficeInfo key={c.email} data={c} />
             ))}
 
-            <div className="flex flex-col sm:flex-row gap-8 sm:gap-12">
-              <div className="text-[#848484] text-xs">
-                <h4 className="font-semibold mb-3 text-black">
-                  {t("working.title")}
-                </h4>
-                <p className="text-black">{t("working.days")}</p>
-                <p>{t("working.reception")}</p>
-                <p>{t("working.issuance")}</p>
-                <p>{t("working.lunch")}</p>
+            <div className="flex flex-col gap-8 sm:flex-row sm:gap-12">
+              <div className="text-xs text-[#848484]">
+                <h4 className="mb-3 font-semibold text-black">{t('working.title')}</h4>
+                <p className="text-black">{t('working.days')}</p>
+                <p>{t('working.reception')}</p>
+                <p>{t('working.issuance')}</p>
+                <p>{t('working.lunch')}</p>
               </div>
-              <div className="text-[#848484] text-xs">
-                <h4 className="font-semibold mb-3 text-black">
-                  {" "}
-                  {t("daysOff.title")}
-                </h4>
-                <p className="text-black">{t("daysOff.days")}</p>
-                <p className="text-black">{t("daysOff.andHolidays")}</p>
+              <div className="text-xs text-[#848484]">
+                <h4 className="mb-3 font-semibold text-black"> {t('daysOff.title')}</h4>
+                <p className="text-black">{t('daysOff.days')}</p>
+                <p className="text-black">{t('daysOff.andHolidays')}</p>
               </div>
             </div>
           </div>
